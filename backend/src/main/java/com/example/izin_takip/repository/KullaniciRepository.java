e29b27b07236ef230fe4390670c69d30eb3a81d9
bbeyzacagli <beyzacagli2@gmail.com>
2024-08-09 18:05:36 +0300

    update

--- backend/src/main/java/com/example/izin_takip/repository/KullaniciRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.izin_takip.repository;
-
-import com.example.izin_takip.models.Kullanici;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KullaniciRepository extends JpaRepository<Kullanici, Long> {
-    Kullanici findByKullaniciAdi(String kullaniciAdi);
-}
